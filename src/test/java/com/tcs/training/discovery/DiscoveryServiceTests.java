156e712ab85d4fa8edef62fa3133fa80366a020b
arghyagiri <arghya940609rkm@gmail.com>
2023-10-30 22:54:03 +0000

    initial commit

---
+++ src/test/java/com/tcs/training/discovery/DiscoveryServiceTests.java
@@ -0,0 +1,13 @@
+package com.tcs.training.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
